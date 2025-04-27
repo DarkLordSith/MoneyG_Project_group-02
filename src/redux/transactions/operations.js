34ac4b4434f3aef8f8f6bdd7063ea3e3e9aa1c1e
MarinaV27 <Vasilishinamari@gmail.com>
2025-04-27 13:51:51 +0300

    fix conflicts

--- src/redux/transactions/operations.js
+++ src/redux/transactions/operations.js
@@ -72,6 +72,7 @@ export const fetchSummary = createAsyncThunk(
   }
 );
 
+
 // fetchCategories
 export const fetchCategories = createAsyncThunk(
   "transactions/fetchCategories",
@@ -90,57 +91,23 @@ export const fetchCategories = createAsyncThunk(
   }
 );
 
-//export const fetchCategories = createAsyncThunk(
-//  "transactions/fetchCategories",
-//  async ({ month, year }, thunkAPI) => {
-//    prepareAuthHeader(thunkAPI);
-//    try {
-//      thunkAPI.dispatch(setIsLoading(true));
-//      const response = await axios.get("/transactions/summary", {
-//        params: { month, year },
- //     });
-//      return response.data;
-//    } catch (error) {
-//      return thunkAPI.rejectWithValue(error.message);
-//    } finally {
-//      thunkAPI.dispatch(setIsLoading(false));
-//    }
-//  }
-//);
+// Закомментированное из ветки main, оставь это, если хочешь оставить старую версию для будущего использования:
+/*
+export const fetchCategories = createAsyncThunk(
+  "transactions/fetchCategories",
+  async ({ month, year }, thunkAPI) => {
+    try {
+      thunkAPI.dispatch(setIsLoading(true));
+      const response = await axiosInstance.get("/transactions/summary", {
+        params: { month, year },
+      });
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    } finally {
+      thunkAPI.dispatch(setIsLoading(false));
+    }
+  }
+);
+*/
 
-//export const fetchCategories = createAsyncThunk(
-//  "transactions/fetchCategories",
-//  async ({ month, year }, thunkAPI) => {
-//    prepareAuthHeader(thunkAPI);
-//    try {
-//      thunkAPI.dispatch(setIsLoading(true));
-//      const response = await axios.get("/transactions/summary", {
-//        params: { month, year },
-//      });
-//
-//      const { income, expenses } = response.data;
-//
-//      const incomeCategories = income?.categories || [];
-//      const expenseCategories = expenses?.categories || [];
-//
-//      const allCategories = [...incomeCategories, ...expenseCategories];
-//
-//      // Витягуємо імена категорій
-//      const categoryNames = allCategories.map((cat) => ({
-//        id: cat.id || cat._id || cat.name, // запасний варіант для id
-//        name: cat.name,
-//      }));
-//
-//      const uniqueCategories = [
-//        ...new Map(categoryNames.map((item) => [item.name, item])).values(),
-//      ];
-//
-//      return uniqueCategories;
-//
-//    } catch (error) {
-//      return thunkAPI.rejectWithValue(error.message);
-//    } finally {
-//      thunkAPI.dispatch(setIsLoading(false));
-//    }
-//  }
-//);
