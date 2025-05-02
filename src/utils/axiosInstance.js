bc844fe3423f04863a7edab40f19ac419bf9cadd
Vladimir Tryasunov <tryasunov933@gmail.com>
2025-05-02 12:05:47 +0300

    add axios.interceptor to axiosInstace.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,74 @@
 import axios from "axios";
+import { setAuthToken, getAuthToken } from "./authToken";
+import { store } from "../redux/store";
+import { logout } from "../redux/auth/operations";
 
 const axiosInstance = axios.create({
   baseURL: "https://money-guard-backend-lnfk.onrender.com",
   withCredentials: true,
 });
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) prom.reject(error);
+    else prom.resolve(token);
+  });
+  failedQueue = [];
+};
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      getAuthToken()
+    ) {
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        console.log("[⏳ AXIOS] Waiting for token refresh...");
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        }).then((token) => {
+          originalRequest.headers["Authorization"] = `Bearer ${token}`;
+          return axiosInstance(originalRequest);
+        });
+      }
+
+      isRefreshing = true;
+
+      try {
+        const res = await axios.post(
+          "https://money-guard-backend-lnfk.onrender.com/auth/refresh",
+          {},
+          { withCredentials: true }
+        );
+
+        const newToken = res.data.data.accessToken;
+        setAuthToken(newToken);
+        processQueue(null, newToken);
+
+        originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        processQueue(err, null);
+        store.dispatch(logout());
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
