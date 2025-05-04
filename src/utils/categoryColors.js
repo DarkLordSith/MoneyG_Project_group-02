baa6ae3cb8be1ab8ff380ffc19426b774ab77043
MarinaV27 <Vasilishinamari@gmail.com>
2025-05-04 21:33:44 +0300

    fix/colors chart

--- src/utils/categoryColors.js
+++ src/utils/categoryColors.js
@@ -3,14 +3,23 @@ const BASE_COLORS = [
   '#4A56E2', '#81E1FF', '#24CCA7', '#00AD84', '#D35400'
 ];
 
+const categoryColors = {};
+
 const getColorByCategory = (name) => {
   if (!name) return '#ccc';
-  // Простой хеш по строке
+ 
+if (categoryColors[name]) return categoryColors[name];
+
   let hash = 0;
   for (let i = 0; i < name.length; i++) {
     hash = name.charCodeAt(i) + ((hash << 5) - hash);
   }
   const index = Math.abs(hash) % BASE_COLORS.length;
+
+  const color = BASE_COLORS[index];
+  categoryColors[name] = color;
+
+
   return BASE_COLORS[index];
 };
 
